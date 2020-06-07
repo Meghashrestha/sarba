c261927614220af11065b72df7df1f8da8742bc0
megha <meghashrestha30@gmail.com>
2020-06-07 21:41:45 +0545

    first commit

---
+++ js/custom.js
@@ -0,0 +1,190 @@
+jQuery(document).ready(function($) {
+
+	$('.scrollup').click(function(){
+		$("html, body").animate({ scrollTop: 0 }, 1000);
+		return false;
+	});
+	
+		$('.accordion').on('show', function (e) {
+		
+			$(e.target).prev('.accordion-heading').find('.accordion-toggle').addClass('active');
+			$(e.target).prev('.accordion-heading').find('.accordion-toggle i').removeClass('icon-plus');
+			$(e.target).prev('.accordion-heading').find('.accordion-toggle i').addClass('icon-minus');
+		});
+		
+		$('.accordion').on('hide', function (e) {
+			$(this).find('.accordion-toggle').not($(e.target)).removeClass('active');
+			$(this).find('.accordion-toggle i').not($(e.target)).removeClass('icon-minus');
+			$(this).find('.accordion-toggle i').not($(e.target)).addClass('icon-plus');
+		});	
+
+	$('.navigation').onePageNav({
+		begin: function() {
+			console.log('start');
+		},
+		end: function() {
+			console.log('stop');
+		},
+			scrollOffset: 0		
+	});
+	
+	// prettyPhoto
+	$("a[data-pretty^='prettyPhoto']").prettyPhoto();		
+
+    // Localscrolling 
+	$('#menu-main, .brand').localScroll();
+	
+	$('#menu-main li a').click(function(){
+		var links = $('#menu-main li a');
+		links.removeClass('selected');
+		$(this).addClass('selected');
+	});
+
+    var iOS = false,
+        p = navigator.platform;
+
+    if (p === 'iPad' || p === 'iPhone' || p === 'iPod') {
+        iOS = true;
+    }	
+	
+    if (iOS === false) {
+
+        $('.flyIn').bind('inview', function (event, visible) {
+            if (visible === true) {
+                $(this).addClass('animated fadeInUp');
+            }
+        });
+
+        $('.flyLeft').bind('inview', function (event, visible) {
+            if (visible === true) {
+                $(this).addClass('animated fadeInLeftBig');
+            }
+        });
+
+        $('.flyRight').bind('inview', function (event, visible) {
+            if (visible === true) {
+                $(this).addClass('animated fadeInRightBig');
+            }
+        });
+
+    }
+	
+	// add animation on hover
+		$(".service-box").hover(
+			function () {
+			$(this).find('img').addClass("animated pulse");
+			$(this).find('h2').addClass("animated fadeInUp");
+			},
+			function () {
+			$(this).find('img').removeClass("animated pulse");
+			$(this).find('h2').removeClass("animated fadeInUp");
+			}
+		);
+		
+	
+	// cache container
+	var $container = $('#portfolio-wrap');
+	$.browser.safari = ($.browser.webkit && !(/chrome/.test(navigator.userAgent.toLowerCase())));	
+	
+	if($.browser.safari){ 	
+	// initialize isotope
+	$container.isotope({
+		animationEngine : 'jquery',
+		animationOptions: {
+			duration: 200,
+			queue: false
+		},
+		layoutMode: 'fitRows'
+	});
+	} else {	
+	$container.isotope({
+		animationEngine : 'best-available',
+		animationOptions: {
+			duration: 200,
+			queue: false
+		},
+		layoutMode: 'fitRows'
+	});	
+	
+	$(window).resize(function() {
+		$container.isotope('reLayout');
+	});
+	}
+	// filter items when filter link is clicked
+	$('#filters a').click(function(){
+		$('#filters a').removeClass('active');
+		$(this).addClass('active');
+		var selector = $(this).attr('data-filter');
+		$container.isotope({ filter: selector });
+		return false;
+	});
+
+	// flexslider main
+	$('#main-flexslider').flexslider({						
+		animation: "swing",
+		direction: "vertical", 
+		slideshow: true,
+		slideshowSpeed: 3500,
+		animationDuration: 1000,
+		directionNav: true,
+		prevText: '<i class="icon-angle-up icon-2x"></i>',       
+		nextText: '<i class="icon-angle-down icon-2x active"></i>', 
+		controlNav: false,
+		smootheHeight:true,						
+		useCSS: false
+	});
+});
+	
+$(window).load(function () {
+    $(".trigger_popup_fricc").click(function(){
+       $('.hover_bkgr_fricc').show();
+    });
+    $('.hover_bkgr_fricc').click(function(){
+        $('.hover_bkgr_fricc').hide();
+    });
+    $('.popupCloseButton').click(function(){
+        $('.hover_bkgr_fricc').hide();
+    });
+});
+
+$(document).ready(function(){
+  buildMap();
+});
+
+var sw = document.body.clientWidth,
+    bp = 550,
+    $map = $('.map');
+var static = "https://maps.google.com/maps/api/staticmap?center=55.7402023,12.5341835&zoom=13&markers=55.7402023,12.5341835&size=640x320&sensor=true";
+var embed = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1766.1469093256646!2d85.33655589128783!3d27.708213097732376!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb1930b3cb6e5b%3A0xa98c94e714388e03!2sSarwanidan%20clinic!5e0!3m2!1sen!2snp!4v1583936920884!5m2!1sen!2snp" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>';
+
+function buildMap() {
+  if(sw>bp) { //If Large Screen
+      if($('.map-container').length < 1) { //If map doesn't already exist
+        buildEmbed();
+      }
+  } else {
+      if($('.static-img').length < 1) { //If static image doesn't exist
+        buildStatic();
+      }
+  }
+};
+
+function buildEmbed() { //Build iframe view
+    $('<div class="map-container"/>').html(embed).prependTo($map);
+};
+  
+function buildStatic() { //Build static map
+   var mapLink = $('.map-link').attr('href'),
+       $img = $('<img class="static-img" />').attr('src',static);
+   $('<a/>').attr('href',mapLink).html($img).prependTo($map); 
+}
+
+$(window).resize(function() {
+  sw = document.body.clientWidth;
+  buildMap();
+  google.maps.event.trigger(map, "resize");
+});
+   
+
+
+
